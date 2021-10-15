81824fbe9732097dc23b303436365577a3ffce67
MichelleTabby <69860641+MichelleTabby@users.noreply.github.com>
2021-10-15 11:35:11 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    printf("\t County library management system\n");
+    printf("1:Login\n");
+    printf("Welcome Michelle\n");
+    printf("\tWhat do you want to do?\n");
+    printf("\t2:Add a user\n");
+    printf("\t3:Add a book\n");
+    printf("\t4:Borrow a book\n");
+    printf("\t5:Return book\n");
+    printf("\t6:Replace a book\n");
+    printf("\t7:Update User\n");
+    printf("\t8:Delete user\n");
+    printf("9:Logout\n");
+    printf("\tGood bye!!\n");
+    
+
+    return 0;
+}
